2a9c1d9d426a6bb0e2bcf013765df845d7accd3e
Jacopo Boffelli <32513386+Jacopo98@users.noreply.github.com>
2018-05-18 13:10:17 +0200

    Create Esame.java

---
+++ Esercitazione_7/Esercizio_39-overloading/Esame.java
@@ -0,0 +1,17 @@
+package Esame;
+
+public class Esame {
+
+	String nome;
+	int voto;
+	
+	public Esame(String nome, int voto){
+		this.nome=nome;
+		this.voto=voto;
+	}
+	
+	public int getVoto() {
+		return voto;
+	}
+	
+}
